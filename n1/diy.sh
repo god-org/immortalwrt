fb8cf559925ce1630b58087e81e711ec5f5a4058
zzzzzzqqq8 <71330272+zzzzzzqqq8@users.noreply.github.com>
2026-01-31 11:04:28 +0800

    Commit: 2026-01-31 11:04:27

--- n1/diy.sh
+++ n1/diy.sh
@@ -4,7 +4,7 @@
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
 # Git sparse clone
-git_sparse_clone() {
+function git_sparse_clone() {
     branch="$1" repourl="$2" && shift 2
     git clone --depth=1 -b "$branch" --single-branch --filter=blob:none --sparse "$repourl"
     repodir=$(echo "$repourl" | awk -F '/' '{print $(NF)}')
